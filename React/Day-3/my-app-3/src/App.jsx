5f5850185b90434ebcc4b2ea9eacbda63a571aa7
karthick <karthickkavin004@gmail.com>
2023-08-21 10:41:01 +0530

    KavinM

---
+++ React/Day-3/my-app-3/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+    const m=10;
+    const n=20;
+  return (
+    <>
+      <div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ React/Day-3/my-app-3/src/Home.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <div>
+    <div class="wrapper">
+       <div class="table basic">
+           <div class="price-section">
+               <div class="price-area">
+                   <div class="inner-area">
+                       <span class="text">
+                         &#8377;
+                       </span>
+                       <span class="price">04</span>
+                   </div>
+               </div>
+           </div>
+           <div class="package-name">
+    
+           </div>
+           <div class="features">
+               <li>
+                   <span class="list-name">Single User</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">50GB Storage</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Unlimited Public projects</span>
+                   <span class="icon cross"><i class="far fa-times-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Community Access</span>
+                   <span class="icon cross"><i class="far fa-times-circle"></i></span>
+               </li>
+               <div class="btn"><button>Buy</button></div>
+           </div>
+       </div>
+       <div class="table Premium">
+           <div class="price-section">
+               <div class="price-area">
+                   <div class="inner-area">
+                       <span class="text">
+                         &#8377;
+                       </span>
+                       <span class="price">24</span>
+                   </div>
+               </div>
+           </div>
+           <div class="package-name">
+            
+           </div>
+           <div class="features">
+               <li>
+                   <span class="list-name">Five Users</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Unlimited Private Projects</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Dedicated Phone Support</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Free Subdomain</span>
+                   <span class="icon cross"><i class="far fa-times-circle"></i></span>
+               </li>
+               <div class="btn"><button>Buy</button></div>
+           </div>
+       </div>
+       <div class="table Ultimate">
+           <div class="price-section">
+               <div class="price-area">
+                   <div class="inner-area">
+                       <span class="text">
+                          &#8377;
+                       </span>
+                       <span class="price">44</span>
+                   </div>
+               </div>
+           </div>
+           <div class="package-name">
+               
+           </div>
+           <div class="features">
+               <li>
+                   <span class="list-name">Unlimed Users</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Unlimted Storage</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Unlimted Extensions</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Monthly Status Reports</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <div class="btn"><button>Buy</button></div>
+           </div>
+       </div>
+   </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
