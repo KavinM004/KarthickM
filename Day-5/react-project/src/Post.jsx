f048eab50b49bbeb5554e64c947a3e63d6d92bf6
karthick <karthickkavin004@gmail.com>
2023-08-23 10:20:53 +0530

    KavinM

---
+++ Day-5/react-project/src/Post.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+function Post() {
+    
+  return (
+    <>
+    <div id='body'>
+    <div class="Cart-Container">
+    <div class="Header">
+      <h3 class='Heading'>Shopping Cart</h3>
+       <h5 class='Action'>Remove all</h5>
+ </div>
+ <div class='Cart-Items'>
+ <div class='image-box'>
+ <img src='images/apple.png' style={{ height:'120px' }} />
+ </div>
+ <div class='about'>
+ <h1 class='title'>Apple Juice</h1>
+ <h3 class='subtitle'>250ml</h3>
+ <img src='images/veg.png' style={{ height:'30px' }}/>
+ </div>
+ <div class='counter'>
+ {/* <div class='btn'>+</div> */}
+ <button>+</button>
+ <div class='count'>2</div>
+ {/* <div class='btn'>-</div> */}
+ <button>-</button>
+ </div>
+ <div class='prices'>
+ <div class='amount'>$2.99</div>
+ <div class='save'><u>Save for later</u></div>
+ {/* <div class='remove'><u>Remove</u></div> */}
+ <button className='remove'>Remove</button>
+ </div>
+ </div>
+ <div class='Cart-Items'>
+ <div class='image-box'>
+ <img src='images/apple.png' style={{ height:'120px' }} />
+ </div>
+ <div class='about'>
+ <h1 class='title'>Orange Juice</h1>
+ <h3 class='subtitle'>250ml</h3>
+ <img src='images/veg.png' style={{ height:'30px' }}/>
+ </div>
+ <div class='counter'>
+ {/* <div class='btn'>+</div> */}
+ <button>+</button>
+ <div class='count'>1</div>
+ {/* <div class='btn'>-</div> */}
+ <button>-</button>
+ </div>
+ <div class='prices'>
+ <div class='amount'>$3.19</div>
+ <div class='save'><u>Save for later</u></div>
+ {/* <div class='remove'><u>Remove</u></div> */}
+ <button className='remove'>Remove</button>
+ </div>
+ </div>
+ <hr /> 
+ <div class='checkout'>
+ <div class='total'>
+ <div>
+ <div class='Subtotal'>Sub-Total</div>
+ <div class='items'>Cart : 2 items</div>
+ </div>
+ <div class='total-amount'>$6.18</div>
+ </div>
+ <button class='button'>Checkout</button>
+ </div>
+    </div>
+    </div>
+    
+    
+    </>
+  )
+}
+
+export default Post
