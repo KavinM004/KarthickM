093e4f18b181a848a7ba338d29365f9dbf1dea8c
karthick <karthickkavin004@gmail.com>
2023-09-17 20:21:34 +0530

    MongoDB

---
+++ playground-1.mongodb.js
@@ -0,0 +1,22 @@
+use("school");
+// db.students.find({name : "Kavin M"},{age : 21, _id : 0});
+// db.getMongo().getDBs();
+// db.getCollectionNames();
+
+// db.students.insertMany([
+//     {
+//         "_id":1,name : 'Guvi'
+//     },
+//     {
+//         "_id":2,name : 'geek',goal : { "street":"nehru","pincode":"637102"}
+//     },
+//     {
+//         name : "sjsj",Age : 32,hobbies : [ "Drawing","Dancing","Playing"]
+//     }
+// ]);
+
+// db.students.insertOne({
+//     name : "Ramesh"
+// });
+
+db.students.find();
