b3fbabfd09f09c6a5e0680daefdb51b5b680c8cb
karthick <karthickkavin004@gmail.com>
2023-09-24 20:39:44 +0530

    commit

--- Node/Server.js
+++ Node/Server.js
@@ -1,20 +1,2 @@
-function operation(a,b,name){
-    switch(name){
-        case 'add':
-            return a+b;
-            break;
-
-        case 'sub':
-            return a-b;
-
-        case 'mul':
-            return a*b;
-
-        case 'div':
-            return a/b;
-    }
-}
-console.log("Final Value : ",operation(10,20,"add"));
-console.log("Final Value : ",operation(10,20,"sub"));
-console.log("Final Value : ",operation(10,20,"mul"));
-console.log("Final Value : ",operation(10,20,"div"));
+const express=require("express");
+const app=express();
