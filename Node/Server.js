db0c3e9546197d6a062a4e9c4884a2aa8e863240
karthick <karthickkavin004@gmail.com>
2023-09-24 22:43:23 +0530

    Server.js

--- Node/Server.js
+++ Node/Server.js
@@ -12,7 +12,7 @@ app.get("/login",(req,res)=>{
     res.send("hello,Kavin-Login");
 });
 app.listen(PORT,()=>{
-    console.log("Server is Currently running on",PORT,"port");
+    console.log("Server is Currently running on localhost :",PORT);
 });
 mongoose.connect('mongodb://127.0.0.1:27017/test')
-  .then(() => console.log('Connected!'));
+  .then(() => console.log('MongoDB Sever is Connected!'));
