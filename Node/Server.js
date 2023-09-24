55116204efcc807d7dfa5190838f27ae9936bae3
karthick <karthickkavin004@gmail.com>
2023-09-24 21:59:10 +0530

    Mongoose

--- Node/Server.js
+++ Node/Server.js
@@ -1,2 +1,18 @@
 const express=require("express");
+const mongoose=require("mongoose");
 const app=express();
+const PORT=3000;
+app.get("/",(req,res)=>{
+    res.send("hello,Kavin");
+});
+app.get("/home",(req,res)=>{
+    res.send("hello,Kavin-home page");
+});
+app.get("/login",(req,res)=>{
+    res.send("hello,Kavin-Login");
+});
+app.listen(PORT,()=>{
+    console.log("Server is Currently running on",PORT,"port");
+});
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
